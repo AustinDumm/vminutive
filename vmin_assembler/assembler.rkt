76a5bc6a67dd31943eec9da7bdd42f4baac06c40
Austin Dumm <austin.dumm@gmail.com>
2023-02-15 17:53:08 -0500

    Add string literals and Include optimizations

--- vmin_assembler/assembler.rkt
+++ vmin_assembler/assembler.rkt
@@ -6,6 +6,21 @@
 (require "assemble-const.rkt")
 (require "assembler-utilities.rkt")
 
+(define (build-vmin-string
+         string)
+  (define byte-list (bytes->list (string->bytes/utf-8 string)))
+  (define len (length byte-list))
+
+  (cons len byte-list))
+
+(define (process-chars-commands
+         instruction-list)
+  (append-map (lambda (instruction)
+                (if (string? instruction)
+                    (build-vmin-string instruction)
+                    (list instruction)))
+              instruction-list))
+
 (define (place-instr-end
          instruction-list)
   (append instruction-list '((DEF-LABEL instr_end))))
@@ -14,9 +29,44 @@
          instruction-list)
   (append instruction-list `((DEF-CONST mem_limit ,(expt 2 32)))))
 
+(define (is-lib-command command)
+  (and (list? command)
+       (eq? 'LIB (first command))))
+
+(define lib-prefix-list (list))
+(define (process-lib-prefixes
+         instruction-list)
+  (append-map (lambda (instruction)
+                (if (is-lib-command instruction)
+                    (begin
+                      (set! lib-prefix-list (append lib-prefix-list (list (list-ref instruction 1))))
+                      '())
+                    (list instruction)))
+              instruction-list))
+  
+
+(define included-set (mutable-set))
 (define (fetch-include instruction)
   (define file-name (list-ref instruction 1))
-  (define file (open-input-file file-name))
+
+  (if (set-member? included-set file-name)
+      '()
+      (begin
+        (set-add! included-set file-name)
+        (grab-include-file file-name))))
+
+(define (grab-include-file file-name)
+  (define (try-open-file file-name prefixes)
+    (if (empty? prefixes)
+        (open-input-file file-name)
+        (with-handlers ([exn:fail:filesystem:errno?
+                         (lambda (_) (try-open-file file-name (rest prefixes)))])
+          (let* ((next-prefix (first prefixes))
+                 (full-file-name (string-append next-prefix "/" file-name))
+                 (input-file (open-input-file full-file-name)))
+            input-file))))
+
+  (define file (try-open-file file-name lib-prefix-list))
   (define file-data (read file))
   (close-input-port file)
   (process-include file-data))
@@ -69,10 +119,12 @@
                           instruction-lookup))
    flatten
    process-label-commands
+   process-chars-commands
    process-const-commands
    place-const-mem-limit
    process-lit-commands
    process-scripts
-   process-include))
+   process-include
+   process-lib-prefixes))
 
 (display (assemble (read)))
